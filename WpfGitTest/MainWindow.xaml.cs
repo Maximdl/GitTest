b84a1fd91a4aca73f5cc9ccf58739485c1c90b9f
Maxim De Lille <maxim.delille@hotmail.com>
2018-09-13 13:54:12 +0200

    Italiaans

--- WpfGitTest/MainWindow.xaml.cs
+++ WpfGitTest/MainWindow.xaml.cs
@@ -30,6 +30,7 @@ namespace WpfGitTest
             lbBericht.Items.Add("Welkom!");
             lbBericht.Items.Add("Welcome!");
             lbBericht.Items.Add("Wilkommen!");
+            lbBericht.Items.Add("Bevenuto");
         }
     }
 }
